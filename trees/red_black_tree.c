43ce929211d18986a7cd657b8ed88a79a83f0f5c
XDA-7 <xaraxsaren@hotmail.com.au>
2019-08-04 10:57:28 +1000

    .

--- trees/red_black_tree.c
+++ trees/red_black_tree.c
@@ -1,47 +1,73 @@
 #include "stdio.h"
 
-#define TREE_MAX_SIZE 1024
+#define TREE_MAX_SIZE 1048575
+#define ARR_SIZE TREE_MAX_SIZE + 1
 #define NIL 0
 #define RED 0
 #define BLACK 1
 
-typedef const char * value_type;
+typedef int value_type;
 
-int parent[TREE_MAX_SIZE];
-int left[TREE_MAX_SIZE];
-int right[TREE_MAX_SIZE];
-int key[TREE_MAX_SIZE];
-value_type value[TREE_MAX_SIZE];
-int colour[TREE_MAX_SIZE];
+int parent[ARR_SIZE];
+int left[ARR_SIZE];
+int right[ARR_SIZE];
+int key[ARR_SIZE];
+value_type value[ARR_SIZE];
+int colour[ARR_SIZE];
 
 int root;
 int tree_size;
 
 void create_tree(int root_key, value_type root_value, value_type nil_value);
 void insert(int insert_key, value_type insert_value);
+void delete(int delete_key);
 void insert_fixup(int insert_index);
-value_type search(int search_key);
+void delete_fixup(int spliced_child_index);
+int search(int search_key);
+int minimum(int root);
+int successor(int index);
 void left_rotate(int node_index);
 void right_rotate(int node_index);
 void swap_child(int former_child, int new_child);
 
 void print_tree();
 
+void manual_input() {
+    printf("begin\n");
+    int option = -1;
+    int input = 0;
+    while (option) {
+        scanf("%d", &option);
+        if (option == 1) {
+            printf("insert\n");
+            scanf("%d", &input);
+            insert(input, input * input);
+        }
+        else if (option == 2) {
+            printf("delete\n");
+            scanf("%d", &input);
+            delete(input);
+        }
+        else if (option == 3) {
+            printf("search\n");
+            scanf("%d", &input);
+            printf("result: %d\n", value[search(input)]);
+        }
+        else if (option == 4) {
+            print_tree();
+        }
+    }
+}
+
 int main() {
-    create_tree(43, "a", "NIL");
-    print_tree();
-    insert(2, "c");
-    print_tree();
-    insert(7, "d");
-    print_tree();
-    insert(11, "b");
-    print_tree();
-    insert(101, "e");
-    print_tree();
-    insert(111, "g");
-    print_tree();
-    insert(120, "f");
-    print_tree();
+    create_tree(0, 0, -1);
+
+    for (int i = 0; i < TREE_MAX_SIZE; i++) {
+        int val = i * 3;
+        insert(val, val * val);
+    }
+
+    manual_input();
 
     printf("done\n");
 }
@@ -99,6 +125,47 @@ void insert(int insert_key, value_type insert_value) {
     }
 }
 
+void delete(int delete_key) {
+    int delete_index = search(delete_key);
+    int spliced_index;
+    if (left[delete_index] == NIL || right[delete_index] == NIL) {
+        spliced_index = delete_index;
+    }
+    else {
+        spliced_index = successor(delete_index);
+    }
+
+    int spliced_child_index;
+    if (left[spliced_index] != NIL) {
+        spliced_child_index = left[spliced_index];
+    }
+    else {
+        spliced_child_index = right[spliced_index];
+    }
+
+    parent[spliced_child_index] = parent[spliced_index];
+    if (parent[spliced_child_index] == NIL) {
+        root = spliced_child_index;
+    }
+    else {
+        if (spliced_index == left[parent[spliced_index]]) {
+            left[parent[spliced_index]] = spliced_child_index;
+        }
+        else {
+            right[parent[spliced_index]] = spliced_child_index;
+        }
+    }
+
+    if (spliced_index != delete_index) {
+        key[delete_index] = key[spliced_index];
+        value[delete_index] = value[spliced_index];
+    }
+
+    if (colour[spliced_index] == BLACK) {
+        delete_fixup(spliced_child_index);
+    }
+}
+
 void recolour_ancestors_and_move_up(int *current_index, int uncle) {
     colour[uncle] = BLACK;
     colour[parent[*current_index]] = BLACK;
@@ -146,12 +213,75 @@ void insert_fixup(int insert_index) {
     colour[root] = BLACK;
 }
 
-value_type search(int search_key) {
+void delete_fixup(int recolour_index) {
+    while (recolour_index != root && colour[recolour_index] == BLACK) {
+        if (recolour_index == left[parent[recolour_index]]) {
+            int sibling = right[parent[recolour_index]];
+            if (colour[sibling] == RED) {
+                colour[sibling] = BLACK;
+                colour[parent[recolour_index]] = RED;
+                left_rotate(parent[recolour_index]);
+                sibling = right[parent[recolour_index]];
+            }
+
+            if (colour[left[sibling]] == BLACK && colour[right[sibling]] == BLACK) {
+                colour[sibling] = RED;
+                recolour_index = parent[recolour_index];
+            }
+            else {
+                if (colour[right[sibling]] == BLACK) {
+                    colour[left[sibling]] = BLACK;
+                    colour[sibling] = RED;
+                    right_rotate(sibling);
+                    sibling = right[parent[recolour_index]];
+                }
+
+                colour[sibling] = colour[parent[recolour_index]];
+                colour[parent[recolour_index]] = BLACK;
+                colour[right[sibling]] = BLACK;
+                left_rotate(parent[recolour_index]);
+                recolour_index = root;
+            }
+        }
+        else {
+            int sibling = left[parent[recolour_index]];
+            if (colour[sibling] == RED) {
+                colour[sibling] = BLACK;
+                colour[parent[recolour_index]] = RED;
+                right_rotate(parent[recolour_index]);
+                sibling = left[parent[recolour_index]];
+            }
+
+            if (colour[left[sibling]] == BLACK && colour[right[sibling]] == BLACK) {
+                colour[sibling] = RED;
+                recolour_index = parent[recolour_index];
+            }
+            else {
+                if (colour[left[sibling]] == BLACK) {
+                    colour[right[sibling]] = BLACK;
+                    colour[sibling] = RED;
+                    left_rotate(sibling);
+                    sibling = left[parent[recolour_index]];
+                }
+
+                colour[sibling] = colour[parent[recolour_index]];
+                colour[parent[recolour_index]] = BLACK;
+                colour[left[sibling]] = BLACK;
+                right_rotate(parent[recolour_index]);
+                recolour_index = root;
+            }
+        }
+    }
+
+    colour[recolour_index] = BLACK;
+}
+
+int search(int search_key) {
     int current_index = root;
     int current_key = key[root];
     while (current_index != NIL) {
         if (current_key == search_key) {
-            return value[current_index];
+            return current_index;
         }
         else if (current_key > search_key) {
             current_index = left[current_index];
@@ -163,7 +293,29 @@ value_type search(int search_key) {
         }
     }
 
-    return value[NIL];
+    return NIL;
+}
+
+int minimum(int root) {
+    while (left[root] != NIL) {
+        root = left[root];
+    }
+
+    return root;
+}
+
+int successor(int index) {
+    if (right[index] != NIL) {
+        return minimum(right[index]);
+    }
+
+    int parent_index = parent[index];
+    while (parent_index != NIL && index == right[parent_index]) {
+        index = parent_index;
+        parent_index = parent[index];
+    }
+
+    return parent_index;
 }
 
 /*
